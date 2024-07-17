75a7cac06c599775d56208c754f60399baad7c69
miroslav130908 <miroslav1309082008@yandex.com>
2024-07-17 15:27:20 +0300

    Project start

---
+++ react-learning/src/App.js
@@ -0,0 +1,14 @@
+// import libs:
+import React, {useState} from 'react';
+import './App.css';
+
+
+const App = () => {
+    return (
+        <div>
+          <h1>Измените строку ниже и число умножится на 2</h1>
+        </div>
+    );
+};
+
+export default App;
