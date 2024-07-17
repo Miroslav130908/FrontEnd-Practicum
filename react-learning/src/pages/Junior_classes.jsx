59f7d1883c19a36d6e2496063e82c7b3b80b724a
levbern <lev.bernshteyn@gmail.com>
2024-07-17 16:42:32 +0300

    updjuniorclasses

---
+++ react-learning/src/pages/Junior_classes.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+
+const Junior_clases = () => {
+    return (
+        <Fragment>
+            <head>
+                <title>ЛМШ</title>
+            </head>
+            <body>
+                
+            </body>
+        </Fragment>
+    );
+};
+
+export default Junior_clases;
