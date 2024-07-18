14ea1928e27fc8a8813b61db4bd83adb9f01def9
CaptainLevi2008 <albertmin2008@gmail.com>
2024-07-18 16:04:31 +0300

    Header

--- react-learning/src/components/Header/Header.jsx
+++ react-learning/src/components/Header/Header.jsx
@@ -1,22 +1,23 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
+import './Header.css'
+
 const Header = () => {
     return(
         <div class = "head">
-        <div class = "right_buttons">
-            <a class = "info"  aria-label="Инфо" title="Инфо" href="/">
-                Инфо  
-            </a>
-            <a class = "Juniors"  aria-label="Juniors" title="Juniors" href="/junior">
-                6 - 8 классы
-            </a>
-            <a class = "Seniors"  aria-label="Seniors" title="Seniors" href="">
-                9 - 10 классы  
-            </a>
-        
+            <div class = "right_buttons">
+                <a class = "Info"  aria-label="Инфо" title="Инфо" href="/">
+                    Инфо  
+                </a>
+                <a class = "Juniors"  aria-label="Juniors" title="Juniors" href="/junior">
+                    6 - 8 классы
+                </a>
+                <a class = "Seniors"  aria-label="Seniors" title="Seniors" href="">
+                    9 - 10 классы  
+                </a>
+            </div>
         </div>
-    </div>
     )
 };
 
